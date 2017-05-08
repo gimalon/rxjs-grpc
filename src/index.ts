2bc58276f1c0a61a178b95011263a33d02cbe22a
Manuel <manuel@gimalon.com>
2017-05-08 16:52:32 +0200

    add some Metadata for errors.
    This is a first try. Could be nicer i.e a specific ErrorType/Class.
    Maybe instead of current approach JSON stringify entire error object
    could also make sense

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,78 @@
+import * as os from 'os';
 import * as grpc from 'grpc';
 import { Observable } from 'rxjs/Observable';
 
 type DynamicMethods = { [name: string]: any; };
+
+
+
+/**
+ *  Adds the grpc Metadata to an error object with the erver stack
+ *
+ * @param {*} error
+ * @returns {*} error with property metadata
+ */
+function addErrorMetaData(error: any): any {
+  try {
+    if (!error.metadata) {
+      error.metadata = new grpc.Metadata;
+    }
+
+    // if custom error already has a emtadata member remember it
+    let serverMetadata = null;
+    if (! (error.metadata instanceof grpc.Metadata) ) {
+      serverMetadata = error.metadata;
+      error.metadata = new grpc.Metadata();
+    }
+
+    // now set server error metadata
+    error.metadata.add('server-error', error.name);
+    error.metadata.add('server-message', error.message);
+
+    if ( error.stack ) {
+      if        (typeof error.stack.join === 'function') {
+        error.metadata.add('server-stack-bin', Buffer.from(error.stack.join('\n')));
+      } else if (typeof error.stack === 'string') {
+        error.metadata.add('server-stack-bin', Buffer.from(error.stack));
+      } else {
+        error.metadata.add('server-stack-bin', Buffer.from(error.stack + '') );
+      }
+    }
+
+    error.metadata.add('server-name', os.hostname());
+
+    // now json stringifie oroginal metadata. may throw circular error
+    if ( serverMetadata ) {
+      error.metadata.add('serverMetadata-bin', Buffer.from(JSON.stringify(serverMetadata, null, ' ')));
+    }
+
+    return error;
+
+  } catch (e) {
+    return error;
+  }
+
+}
+
+function convertErrorMetaData(error: any): any {
+  let newMetadata: any = {};
+
+  if (error.metadata instanceof grpc.Metadata) {
+    let keyMap = error.metadata.getMap();
+    for ( let key in keyMap) {
+      if (typeof keyMap[key] === 'string') {
+        newMetadata[key] = keyMap[key];
+      } else if (keyMap[key] instanceof Buffer) {
+        newMetadata[key.replace('-bin', '')] = keyMap[key].toString();
+      }
+    }
+
+    error.metadata = newMetadata;
+  }
+
+  return error;
+}
+
 
 export interface GenericServerBuilder<T> {
   start(address: string, credentials?: any): void;
@@ -49,7 +120,7 @@ function createUnaryMethod(rxImpl: DynamicMethods, name: string) {
     const response: Observable<any> = rxImpl[name](call.request);
     response.subscribe(
       data => callback(null, data),
-      error => callback(error)
+      error => callback(addErrorMetaData(error))
     );
   };
 }
@@ -57,7 +128,11 @@ function createUnaryMethod(rxImpl: DynamicMethods, name: string) {
 function createStreamingMethod(rxImpl: DynamicMethods, name: string) {
   return async function(call: any, callback: any) {
     const response: Observable<any> = rxImpl[name](call.request);
-    await response.forEach(data => call.write(data));
+    try {
+      await response.forEach(data => call.write(data));
+    } catch (e) {
+      throw addErrorMetaData(e);
+    }
     call.end();
   };
 }
@@ -113,7 +188,7 @@ function createUnaryClientMethod(grpcClient: DynamicMethods, name: string) {
     return new Observable(observer => {
       grpcClient[name](...args, (error: any, data: any) => {
         if (error) {
-          observer.error(error);
+          observer.error(convertErrorMetaData(error));
         } else {
           observer.next(data);
         }
@@ -128,7 +203,7 @@ function createStreamingClientMethod(grpcClient: DynamicMethods, name: string) {
     return new Observable(observer => {
       const call = grpcClient[name](...args);
       call.on('data', (data: any) => observer.next(data));
-      call.on('error', (error: any) => observer.error(error));
+      call.on('error', (error: any) => observer.error(convertErrorMetaData(error)));
       call.on('end', () => observer.complete());
     });
   };
